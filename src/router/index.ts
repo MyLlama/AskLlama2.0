3eac937a5eff8af65769ac08e2839ac95dcca689
himansh <sai.himanshum011@gmail.com>
2023-09-27 19:33:42 +0530

    Add router for admin page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,34 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import HomePage from "../views/HomePage.vue";
 import Master from "../views/Masters.vue";
+import AdminView from "../views/AdminView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
+    path: "/home",
+    name: "Home",
+    component: HomePage,
   },
-{
-    path:'/master',
-    name:'master',
-    component:Master
-  
-    }
-]
+  {
+    path: "/master",
+    name: "master",
+    component: Master,
+  },
+  {
+    path: "/admin-insights",
+    name: "admin",
+    component: AdminView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
